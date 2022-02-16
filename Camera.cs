b977e7c28106a8e24cc5f791ce789173dfe0b492
David <git@davidmannes.de>
2022-02-16 22:26:19 +0100

    Simplify input handling for camera

--- Camera.cs
+++ Camera.cs
@@ -6,8 +6,6 @@ namespace CityBuilder
 {
 	public class Camera : Camera2D
 	{
-		private Vector2 _direction;
-
 		[Export(PropertyHint.Range, "0, 1024")]
 		public int Speed { get; private set; }
 
@@ -61,52 +59,17 @@ namespace CityBuilder
 			{
 				Zoom /= 1 - ZoomStep;
 			}
-
-
-			else if (@event.IsActionPressed(InputAction.CameraUp))
-			{
-				_direction.y = -1;
-			}
-
-			else if (@event.IsActionReleased(InputAction.CameraUp))
-			{
-				_direction.y = 0;
-			}
-
-			else if (@event.IsActionPressed(InputAction.CameraDown))
-			{
-				_direction.y = 1;
-			}
-
-			else if (@event.IsActionReleased(InputAction.CameraDown))
-			{
-				_direction.y = 0;
-			}
-
-			else if (@event.IsActionPressed(InputAction.CameraLeft))
-			{
-				_direction.x = -1;
-			}
-
-			else if (@event.IsActionReleased(InputAction.CameraLeft))
-			{
-				_direction.x = 0;
-			}
-
-			else if (@event.IsActionPressed(InputAction.CameraRight))
-			{
-				_direction.x = 1;
-			}
-
-			else if (@event.IsActionReleased(InputAction.CameraRight))
-			{
-				_direction.x = 0;
-			}
 		}
 
 		public override void _Process(float delta)
 		{
-			Position += Speed * delta * _direction;
+			var direction = Input.GetVector(
+				InputAction.CameraLeft,
+				InputAction.CameraRight,
+				InputAction.CameraUp,
+				InputAction.CameraDown);
+
+			Position += Speed * delta * direction;
 		}
 	}
 }
